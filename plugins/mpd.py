50697e31ab8dece61e8324ee1133f0837617b9d8
Zach Tatlock <ztatlock@cs.ucsd.edu>
2012-02-22 09:50:03 -0800

    help, synonyms, clean up fmt, pull out TRUNC

--- plugins/mpd.py
+++ plugins/mpd.py
@@ -2,8 +2,10 @@ from util import hook
 from mpd import MPDClient
 
 ## TODO
-##   move
-##   help
+##   mpd password support
+
+# truncate queue and search to avoid flooding
+TRUNC = 5
 
 @hook.command
 def mpd(inp, bot=None, say=None, pm=None):
@@ -21,16 +23,15 @@ def mpd(inp, bot=None, say=None, pm=None):
 
   def fmt(catg, s):
     try:
-      if catg == 'title':
-        return '%s - %s - %s' % (s['artist'], s['album'], s['title'])
-      elif catg == 'album':
-        return '%s - %s' % (s['artist'], s['album'])
-      elif catg == 'artist':
-        return '%s' % (s['artist'])
-      else:
-        return 'Error: unknown catg for fmt'
+      return \
+        { 'title'  : '%s - %s - %s' % (s['artist'], s['album'], s['title'])
+        , 'album'  : '%s - %s' % (s['artist'], s['album'])
+        , 'artist' : s['artist']
+        }[catg]
+    except KeyError:
+      return 'Error: unknown catg "%s" for fmt' % catg
     except:
-      return 'Error: bad artist, album, or title'
+      return 'Error: bad tags in song passed to fmt'
 
   def queue():
     q = con.playlistinfo()
@@ -46,10 +47,10 @@ def mpd(inp, bot=None, say=None, pm=None):
     except:
       i = 0
     pm('Queue (%d total):' % len(q))
-    for s in q[i:i+5]:
+    for s in q[i:i+TRUNC]:
       pm(s)
 
-  def playing():
+  def status():
     x = con.status()
     if x['state'] == 'play':
       s = con.playlistid(x['songid'])[0]
@@ -59,11 +60,14 @@ def mpd(inp, bot=None, say=None, pm=None):
 
   def search(catg, x):
     res = con.search(catg, x)
-    res = [fmt(catg, r) for r in res]
+    if catg == 'artist':
+      res = [fmt('album', r) for r in res]
+    else:
+      res = [fmt(catg, r) for r in res]
     res = list(set(res)) # remove dups
     if res != []:
       pm('Results for %s "%s" (%d total):' % (catg, x, len(res)))
-      for r in res[:5]:
+      for r in res[:TRUNC]:
         pm('  %s' % r)
     else:
       say('Sorry, no results for %s "%s"' % (catg, x))
@@ -90,12 +94,15 @@ def mpd(inp, bot=None, say=None, pm=None):
         say('Removed track %s.' % arg[0])
       except:
         say('Could not remove track %s.' % arg[0])
-    else:
+    elif len(arg) == 2:
       try:
         con.delete('%s:%s' % (arg[0], arg[1]))
         say('Removed tracks %s to %s.' % (arg[0], arg[1]))
       except:
         say('Could not remove tracks %s to %s.' % (arg[0], arg[1]))
+    else:
+      say('Sorry, only "rm" only understands up to 2 arguments.')
+
 
   def clear():
     con.clear()
@@ -105,7 +112,7 @@ def mpd(inp, bot=None, say=None, pm=None):
     con.update()
     say('Updated database.')
 
-  def toggle_play():
+  def tog():
     x = con.status()
     if x['state'] == 'play':
       con.pause('1')
@@ -126,37 +133,78 @@ def mpd(inp, bot=None, say=None, pm=None):
       con.play(arg[0])
       say('Playing %s.' % arg[0])
 
+  def next():
+    con.next()
+    say('Skipped to next song.')
+
+  def mv():
+    if len(arg) == 2:
+      try:
+        con.move(arg[0], arg[1])
+        say('Moved track(s) %s to position %s.' % (arg[0], arg[1]))
+      except:
+        say('Could not move track(s) %s to position %s.' % (arg[0], arg[1]))
+    else:
+      say('Sorry, move only understands 2 arguments. Use M:N for range in first arg')
+
+  def help():
+    cmds = [k[0] for k in sorted(cmd_tab.keys())]
+    say('Available commands: %s' % ' '.join(cmds))
+
+  def syns():
+    if len(arg) == 1:
+      ok = False
+      for k in cmd_tab:
+        if arg[0] in k:
+          xs = ' '.join(['%s' % x for x in k])
+          say('Synonyms for "%s": %s' % (arg[0], xs))
+          ok = True
+          break
+      if not ok:
+        say('Sorry, no idea what "%s" means.' % arg[0])
+    else:
+      say('Sorry, synonyms needs an argument.')
+
+  # long name first for help
   cmd_tab = \
-    { ('q', 'queue',):
+    { ('queue', 'q',):
         lambda x: queue()
-    , ('', 'n', 'now', 'playing',):
-        lambda x: playing()
-    , ('?', '?t', 'search',):
+    , ('status', '',):
+        lambda x: status()
+    , ('search-title', '?t', '?',):
         lambda x: search('title', ' '.join(arg))
-    , ('?a', 'search-album',):
+    , ('search-album', '?a',):
         lambda x: search('album', ' '.join(arg))
-    , ('?A', 'search-artist',):
+    , ('search-artist', '?A',):
         lambda x: search('artist', ' '.join(arg))
-    , ('+', '+t', 'add',):
+    , ('add-title', '+t', '+',):
         lambda x: add(con.search, 'title', ' '.join(arg))
-    , ('+a', 'add-album',):
+    , ('add-album', '+a',):
         lambda x: add(con.search, 'album', ' '.join(arg))
-    , ('+!', '+t!', 'add!',):
+    , ('add-title-strict', '+t!', '+!',):
         lambda x: add(con.find, 'title', ' '.join(arg))
-    , ('+a!', 'add-album!',):
+    , ('add-album-strict', '+a!',):
         lambda x: add(con.find, 'album', ' '.join(arg))
-    , ('-', 'rm', 'remove',):
+    , ('remove', 'rm', '-',):
         lambda x: rm()
     , ('clear',):
         lambda x: clear()
-    , ('u', 'up',):
+    , ('update-db', 'up',):
         lambda x: up()
-    , ('p',):
-        lambda x: toggle_play()
-    , ('play',):
+    , ('toggle', 'tog',):
+        lambda x: tog()
+    , ('play', '|>',):
         lambda x: play()
-    , ('pause',):
+    , ('pause', '||',):
         lambda x: pause()
+    , ('next', '>>',):
+        lambda x: next()
+    , ('move', 'mv',):
+        lambda x: mv()
+    , ('help', 'h',):
+        lambda x: help()
+    , ('synonyms', 'syns',):
+        lambda x: syns()
     }
 
   ok = False
